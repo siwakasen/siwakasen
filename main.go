89cdf78e64acb8faafaaee7a8c711b81ada2d223
siwakasen <siwakasen@gmail.com>
2026-09-27 02:24:58 +0700

    fix(port): use port env, keep fallback 8080

--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
 
 	"github.com/siwakasen/siwakasen/handlers"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	mux := http.NewServeMux()
-	port := 8080
 	fmt.Printf("Listen to port %v", port)
 
 	mux.HandleFunc("/addmoji", handlers.AddMoji)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), mux))
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
